2816c331d345a296b701cd74b25f11a3015cb74f
Arthur Geay <arthurgeay.contact@gmail.com>
2021-12-20 23:07:33 +0100

    :lock: Protect routes with auth middleware

--- start/routes.ts
+++ start/routes.ts
@@ -20,9 +20,11 @@
 
 import Route from "@ioc:Adonis/Core/Route";
 
-Route.get("/", async () => {
-  return { hello: "world" };
-});
-
 Route.post("/login", "SecuritiesController.login");
 Route.post("/logout", "SecuritiesController.logout");
+
+Route.group(() => {
+  Route.get("/", async () => {
+    return { hello: "world" };
+  });
+}).middleware("auth");
