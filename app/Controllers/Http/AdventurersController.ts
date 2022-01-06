67372e7e17951130272f0c9c46b56c37f841ebac
Auriane Labille <45030838+Fanghienne@users.noreply.github.com>
2022-01-06 13:46:37 +0100

    Update AdventurersController.ts

--- app/Controllers/Http/AdventurersController.ts
+++ app/Controllers/Http/AdventurersController.ts
@@ -3,6 +3,34 @@
 import Adventurer from "App/Models/Adventurer"
 
 export default class AdventurersController {
+
+    /**
+   * @swagger
+   * /index:
+   *  get:
+   *   tags:
+   *   - Adventurers
+   *   summary: List of all adventurers
+   *   description: List of all adventurers
+   *   security:
+   *    - bearerAuth: []
+   *   responses:
+   *    '200':
+   *      description: A successful response
+   *      content:
+   *        application/json:
+   *          schema:
+   *            type: object
+   *            properties:
+   *              "type":
+   *                type: string
+   *                example: bearer
+   *              token:
+   *                type: string
+   *                example: epfkemkefmfe
+   *    '400':
+   *     description: User not found
+   */
     public async index() {
         return await Adventurer.query().preload("speciality")
     }
