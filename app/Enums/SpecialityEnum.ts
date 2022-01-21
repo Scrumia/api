5737454f76d434dd95fb3b224d5b790214b9c990
Arthur Geay <arthurgeay.contact@gmail.com>
2022-01-21 11:39:56 +0100

    refactor: speciality enum

--- app/Enums/SpecialityEnum.ts
+++ app/Enums/SpecialityEnum.ts
@@ -1,16 +1,16 @@
-const Enum = require('adonis-enum')
+const Enum = require("adonis-enum");
 
 const SpecialityEnum = new Enum({
-    ARCHER: "archer",
-    BARBARIC: "barbaric",
-    PALADIN: "paladin",
-    ARCANE_MAGE: "arcane mage",
-    PRIEST: "priest",
-    GEOMANCER: "geomancer",
-    ALCHEMIST: "alchemist",
-    BLACK_SMITH: "black smith",
-    ENCHANTING: "enchanting",
-    MESSENGER: "messenger"
-})
+  ARCHER: "archer",
+  BARBARIAN: "barbarian",
+  PALADIN: "paladin",
+  ARCANE_MAGE: "arcane mage",
+  KNIGHT: "knight",
+  GEOMANCER: "geomancer",
+  ALCHEMIST: "alchemist",
+  BLACK_SMITH: "black smith",
+  ENCHANTING: "enchanting",
+  MESSENGER: "messenger",
+});
 
-export default SpecialityEnum
+export default SpecialityEnum;
